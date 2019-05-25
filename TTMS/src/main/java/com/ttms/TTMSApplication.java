efaa3bb73b017875f94f3cdc9d39659326d98331
wubin <1947832562@qq.com>
2019-05-25 19:38:05 +0800

    环境搭建

--- TTMS/src/main/java/com/ttms/TTMSApplication.java
+++ TTMS/src/main/java/com/ttms/TTMSApplication.java
@@ -1,6 +1,6 @@
 package com.ttms;
 
-import org.apache.ibatis.annotations.Mapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
