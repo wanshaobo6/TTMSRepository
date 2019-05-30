bd4f9a1f34128f806cf7758535726927be10ced1
wubin <1947832562@qq.com>
2019-05-30 16:14:33 +0800

    项目完善待测试

--- TTMS/src/main/java/com/ttms/Controller/ProduceManage/CreateProjectController.java
+++ TTMS/src/main/java/com/ttms/Controller/ProduceManage/CreateProjectController.java
@@ -1,10 +1,33 @@
 package com.ttms.Controller.ProduceManage;
 
+import com.ttms.Entity.ProGroup;
+import com.ttms.service.ProductManage.ICreateProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 //----------产品管理->产品->创建产品------------
 @RestController
-@RequestMapping("/producemanage/project/createproduct")
+@RequestMapping("/producemanage/product/createproduct")
 public class CreateProjectController {
+    @Autowired
+    private ICreateProjectService createProjectService;
+    /*
+    *功能描述：查询出所有团号
+    *@author罗占
+    *@Description
+    *Date14:30 2019/5/30
+    *Param
+    *return
+    **/
+    @GetMapping("/queryAllGroups")
+    public ResponseEntity<List<ProGroup>> queryAllGroups(){
+        List<ProGroup> groups = this.createProjectService.queryAllGroups();
+        return ResponseEntity.ok().body(groups);
+
+    }
 }
--- TTMS/src/main/java/com/ttms/Controller/SystemManage/OrganizeManageController.java
+++ TTMS/src/main/java/com/ttms/Controller/SystemManage/OrganizeManageController.java
@@ -1,13 +1,12 @@
 package com.ttms.Controller.SystemManage;
 
+import com.ttms.Entity.SysDepartment;
+import com.ttms.Vo.PageResult;
 import com.ttms.service.SystemManage.SysMenusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //系统管理->用户权限模块->组织机构管理
 @RestController
@@ -30,5 +29,50 @@ public class OrganizeManageController {
                                               @RequestParam String departmentNote, @RequestParam int parentId){
         this.sysMenusService.addDepartment(departmentName,departmentCode,departmentNote,parentId);
         return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    /*
+    *功能描述：分页查询所有部门
+    *@author罗占
+    *@Description
+    *Date8:15 2019/5/30
+    *Param
+    *return
+    **/
+    @GetMapping("/page")
+    public ResponseEntity<PageResult<SysDepartment>> queryDepartment(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                                                     @RequestParam(value = "rows", defaultValue = "5") Integer rows,
+                                                                     @RequestParam(value = "departmentname", required = false) String  departmentname,
+                                                                     @RequestParam(value = "valid",required = false,defaultValue = "true")Boolean valid){
+        PageResult<SysDepartment> result = this.sysMenusService.queryDepartment(page,rows,departmentname,valid);
+        return ResponseEntity.ok().body(result);
+    }
+
+    /*
+    *功能描述：启用或禁用部门
+    *@author罗占
+    *@Description
+    *Date9:04 2019/5/30
+    *Param
+    *return
+    **/
+    @PutMapping("/valid/{id}")
+    public ResponseEntity<Void> updateDepartmentValidOrInvalid(@PathVariable("id") Integer id){
+        this.sysMenusService.updateDepartmentValidOrInvalid(id);
+        return ResponseEntity.ok().body(null);
+    }
+
+    /*
+    *功能描述：修改部门
+    *@author罗占
+    *@Description
+    *Date9:12 2019/5/30
+    *Param
+    *return
+    **/
+    @PutMapping("/update/dartment")
+    public ResponseEntity<Void> updateDepartment(SysDepartment department){
+        this.sysMenusService.updateDepartment(department);
+        return ResponseEntity.ok().body(null);
     }
 }
